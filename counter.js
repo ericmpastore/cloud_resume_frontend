503b44f7ee43154b44cb78c0660bab3c1da85b35
Eric P <epas001@hotmail.com>
2026-03-01 08:50:46 -0500

    Started visitor counter file

---
+++ counter.js
@@ -0,0 +1,10 @@
+/*
+    Visitor Counter Application, 03/01/2026, EPastore
+
+    Prequisites: 
+    - Build database to store visitor counts
+    - Build API to handle communication between app and database
+    - Write Python script for function to implement API
+
+    Application Steps(TBD)
+*/
